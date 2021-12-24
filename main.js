5ca4374d5edd5873e64b79276f2a089d1fdaa317
Virginia Morilla <virginia.morilla@gmail.com>
2021-12-24 09:33:39 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+
+
+        window.onload = function exampleFunction() {
+            var options = {searchable: true};
+            NiceSelect.bind(document.getElementById("searchable-select"), options);
+        }
