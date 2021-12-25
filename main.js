a0d05feaca35aa3989fae6dc2c008d77d03ea151
Virginia Morilla <virginia.morilla@gmail.com>
2021-12-25 11:09:06 +0100

    Previsualizacion PDF

--- main.js
+++ main.js
@@ -4,3 +4,23 @@
             var options = {searchable: true};
             NiceSelect.bind(document.getElementById("searchable-select"), options);
         }
+
+
+        function remove() {
+            var element = document.querySelector(".btn-primary");
+            element.remove();
+          }
+       
+     
+
+         document.querySelector('#file-upload').addEventListener('change', ()=>{ 
+             let pdf = document.querySelector('#file-upload').files[0];
+             let pdfFileUrl = URL.createObjectURL(pdf)
+            document.querySelector('#vistaPrevia').setAttribute('src',pdfFileUrl)
+         })
+         
+          
+     
+
+
+
